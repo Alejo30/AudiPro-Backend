894e6c467b1b40c18564567724afd277bfdc636c
Alejo30 <43772749+Alejo30@users.noreply.github.com>
2020-07-26 19:08:46 -0500

    Initial commit

---
+++ restAudiPro/src/main/java/com/restCalculadora/RestAudiProApplication.java
@@ -0,0 +1,13 @@
+package com.restCalculadora;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestAudiProApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestAudiProApplication.class, args);
+	}
+
+}
---
+++ restAudiPro/src/test/java/com/restCalculadora/RestAudiProApplicationTests.java
@@ -0,0 +1,13 @@
+package com.restCalculadora;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestAudiProApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
