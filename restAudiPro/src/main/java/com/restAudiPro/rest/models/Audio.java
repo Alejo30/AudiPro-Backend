614106e589c4e819736bcb4b3c37ece539186d0e
lusho31 <47617208+lusho31@users.noreply.github.com>
2020-08-04 19:03:07 -0500

    .

--- restAudiPro/src/main/java/com/restAudiPro/rest/models/Audio.java
+++ restAudiPro/src/main/java/com/restAudiPro/rest/models/Audio.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.restAudiPro.rest.models;
 
 import java.io.Serializable;
@@ -27,7 +22,6 @@ public class Audio implements Serializable{
     private String album;
     
     public Audio() {}
-    
     
     
 	public Audio(String nombre, String autor, int anio, String genero, String album) {
